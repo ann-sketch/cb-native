7b6111f9d9dc20b7c378f0f6a91d7184f8b0ce01
Dennis-Lord <dennislord2001@gmail.com>
2021-11-29 14:09:11 +0000

    Test Run 1

--- screens/settingScreen.js
+++ screens/settingScreen.js
@@ -1,15 +1,120 @@
-import React from 'react'
-import { View, Text, Button } from "react-native";
+// import React from 'react'
+// import { View, Text, Button } from "react-native";
 
-// Setting Screen Styles
+// // Setting Screen Styles
+// import styles from '../styles/settingScreen.style';
+
+// export default function SettingScreen({ route }) {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Setting Screen</Text>
+//         {/* Set isLoggedIn state to false when clicked. When isLoggedIn state is false, the welcome page shows automatically designates that the user is not signed in. */}
+//         <Button title="LOGOUT" onPress={() => route.params.setIsLoggedIn(false)} />
+//       </View>
+//     );
+//   }
+
+import React, { useState } from 'react';
+import {View, Text, Switch, Button, Alert} from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faAngleLeft, faAngleRight, faSearch } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/settingScreen.style';
 
-export default function SettingScreen({ route }) {
-    return (
-      <View style={styles.container}>
-        <Text>Setting Screen</Text>
-        {/* Set isLoggedIn state to false when clicked. When isLoggedIn state is false, the welcome page shows automatically designates that the user is not signed in. */}
-        <Button title="LOGOUT" onPress={() => route.params.setIsLoggedIn(false)} />
-      </View>
+const SettingsPage = () => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const [Enabled, setEnabled] = useState(false);
+    const toggleswitch = () => setEnabled(previousState => !previousState);
+
+    const createTwoButtonAlert = () =>
+    Alert.alert(
+      "Logout",
+      "Log out of the application",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel"),
+          style: "cancel"
+        },
+        { text: "Logout", onPress: () => console.log("logout") }
+      ]
     );
-  }
+
+    return (
+        <View style = {styles.sContainer}>
+            <View style={styles.headerC}>
+            <View style={styles.header}>
+                <View style={ styles.vheader}>
+                <FontAwesomeIcon icon={ faAngleLeft } color={ 'black' } size={ 28 }/>
+                <Text style={styles.sT}>Settings</Text>
+                </View>
+                <FontAwesomeIcon icon={faSearch} size={ 22 }/>
+            </View>
+            <View style={styles.profileContainer}>
+                <View>
+                    <Text style={styles.profileText}>JBY cakes</Text>
+                    <Text style={styles.profileText}>example@gmail.com</Text>
+                </View>
+                <View style={styles.profilePicture}></View>
+                </View>
+            </View>
+            <View style={styles.subSection}>
+                <Text style={styles.hT}>GENERAL</Text>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Edit Profile</Text>
+                <FontAwesomeIcon icon={faAngleRight} size={ 24 }/>
+            </View>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Change Password</Text>
+                <FontAwesomeIcon icon={faAngleRight} size={ 24 }/>
+            </View>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Change Language</Text>
+                <FontAwesomeIcon icon={faAngleRight} size={ 24 }/>
+            </View>
+            <Text style={styles.hT}>NOTIFICATIONS</Text>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Notifications</Text>
+                <Switch
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                />
+            </View>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Job Updates</Text>
+                <Switch
+                    trackColor={{ false: "#767577", true: "#81b0ff" }}
+                    thumbColor={Enabled ? "#f5dd4b" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleswitch}
+                    value={Enabled}
+                />
+            </View>
+            <Text style={styles.hT}>TERMS AND SUPPORT</Text>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Terms and Conditions</Text>
+                <FontAwesomeIcon icon={faAngleRight} size={ 24 }/>
+            </View>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Privacy Policy</Text>
+                <FontAwesomeIcon icon={faAngleRight} size={ 24 }/>
+            </View>
+            <View style={styles.sOption}>
+                <Text style={styles.optionsText}>Support</Text>
+                <FontAwesomeIcon icon={faAngleRight} size={ 24 }/>
+            </View>
+            </View>
+            <View style={styles.footer}>
+            <View style={styles.logout} >
+                <Button color='tomato' title="Logout" onPress={createTwoButtonAlert} />
+            </View>
+            </View>
+            
+        </View>
+    )
+};
+
+export default SettingsPage;
--- styles/settingScreen.style.js
+++ styles/settingScreen.style.js
@@ -1,11 +1,190 @@
+// import { StyleSheet } from "react-native";
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+// export default styles;
+
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles  = StyleSheet.create({ 
+//     container: {
+//         width: '100%',
+//         height: '100%',
+//     },   
+//     backButtonContainer: {
+//         marginTop: 50,
+//         marginLeft: 8,
+//     },
+//     profileContainer: {
+//         alignItems: 'center',
+//     },
+//     profileImageBorder: {
+//         width: 100,
+//         height: 100,
+//         borderRadius: '50%',
+//         borderColor: 'grey',
+//         borderBottomWidth: 3,
+//         borderTopWidth: 3,
+//         borderRightWidth: 3,
+//         borderLeftWidth: 3,
+//         marginTop: 20,
+//         marginBottom: 12,
+//         overflow: 'hidden',
+//     },
+//     text: {
+//         fontSize: 26,
+//         marginBottom: 12,
+//         fontWeight: 600
+//     },
+//     subContainer: {
+//         flexWrap: 'wrap',
+//         borderColor: 'black',
+//         borderTopWidth: 1.8,
+//         padding: 24,
+//         flexDirection: 'column',
+//         borderTopRightRadius: 20,
+//         borderTopLeftRadius: 20,
+//     },
+//     textInput: {
+//         fontSize: 26,
+//         backgroundColor: 'gainsboro',
+//         color: 'black',
+//         borderTopLeftRadius: 14,
+//         borderBottomLeftRadius:14,
+//         borderTopRightRadius: 14,
+//         borderBottomRightRadius: 14,
+//         marginTop: 12,
+//         paddingLeft: 20,
+//         paddingRight: 10,
+//         paddingTop: 10,
+//         paddingBottom: 10,
+//         height: 40,
+//         fontSize: 22,
+//     },
+//     notificationContainer: {
+//         display: 'flex',
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         marginRight: 20,
+//         marginLeft: 20,
+//         fontSize: 22,
+//     },
+//     t: {
+//         fontSize: 24,
+//         fontWeight: 500,
+//     }
+// });
 
-export default styles;
+const styles = StyleSheet.create({
+    sContainer: {
+        height: '100%',
+        width: '100%',
+    },
+    headerC: {
+        width: '100%',
+        display: 'flex',
+        backgroundColor: 'dodgerblue',
+        paddingTop: 30,
+        paddingBottom: 8,
+        borderBottomRightRadius: 14,
+        borderBottomLeftRadius: 14,
+    },
+    header: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingRight: 10,
+    },
+    vheader: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+    ,
+    sT: {
+        fontSize: 24,
+        fontWeight: 700,
+        marginLeft: 8,
+        color: 'ghostwhite'
+    },
+    profileContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        alignItems: 'center',
+        marginTop: 12,
+    },
+    profileText: {
+        fontSize: 18,
+        fontWeight: 600,
+        marginLeft: 12,
+        color: 'ghostwhite'
+    },
+    profilePicture: {
+        backgroundColor: 'white',
+        width: 70,
+        height: 70,
+        borderRadius: '50%',
+        marginRight: 18,
+        borderColor: 'whitesmoke',
+        borderBottomWidth: 3,
+        borderTopWidth: 3,
+        borderRightWidth: 3,
+        borderLeftWidth: 3,
+    },
+    subSection: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: 14,
+
+    },
+    hT: {
+        fontWeight: 700,
+        fontSize: 24,
+        marginBottom: 8,
+        marginTop: 8,
+        marginLeft: 12,
+        color: 'black',
+    },
+    sOption: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: 10,
+        borderBottomColor: 'whitesmoke',
+        borderBottomWidth: 4,
+        height: 36,
+    },
+    optionsText: {
+        fontSize: 16,
+        color: 'gray',
+        fontWeight: 500,
+    },
+    logout: {
+        width: 140,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        overflow: 'hidden',
+        backgroundColor: 'tomato',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    footer: {
+        display: 'flex',
+        alignItems: 'center',
+    }
+})
+
+export default styles; 
